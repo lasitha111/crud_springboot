28341c4c1cc7fa3d2634547706ad6660f7c05d0b
Lasitha Priyalal <lasithapriyalal111@gmail.com>
2023-02-23 13:41:08 +0530

    added camelcase function

--- src/main/java/com/mycompany/bar_sale_stat/brands/BrandService.java
+++ src/main/java/com/mycompany/bar_sale_stat/brands/BrandService.java
@@ -3,18 +3,39 @@ package com.mycompany.bar_sale_stat.brands;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
+import org.apache.commons.text.CaseUtils;
 import java.util.List;
 import java.util.Optional;
 
 @Service
 public class BrandService {
-
+    private static final String BLANK_SPACE=" ";
     @Autowired
     private BrandRepositary brandrepo;
 
     public List<Brand> listBrands(){
-        return (List<Brand>) brandrepo.findAll();
+        List<Brand> brands= (List<Brand>) brandrepo.findAll();
+        String brandName;
+        for(int i = 0; i< brands.size();i++){
+            brandName = makeCamelCase(brands.get(i).getBrand_name()) ;
+            brands.get(i).setBrand_name(brandName);
+        }
+        return brands;
+    }
+
+    private String makeCamelCase(String s){
+        String[] words = s.split("\\b");
+        StringBuilder sb = new StringBuilder(words[0].toLowerCase());
+        for (int i = 1; i < words.length; i++) {
+            sb.append(Character.toUpperCase(words[i].charAt(0)));
+            sb.append(words[i].substring(1).toLowerCase());
+            if (i < words.length - 1) {
+                sb.append(" ");
+            }
+        }
+        s = sb.toString();
+        s = Character.toUpperCase(s.charAt(0)) + s.substring(1);
+        return s;
     }
     public void save(Brand brand){
         brandrepo.save(brand);
